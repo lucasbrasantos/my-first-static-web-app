8aea2faaffc31ef2ccbda3187e97bae2d0069462
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-12-05 00:27:28 -0300

    feat: adicionar API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const WelcomeText = () => {
-
 	const value = 'World';
 
 	return (
@@ -13,10 +12,38 @@ export const WelcomeText = () => {
 	)
 }
 
+export const ApiText = () => {
+
+	const [data, setData] = useState('');
+
+	useEffect(() => {
+
+		(async function () {
+			const { text } = await (await fetch(`/api/message`)).json();
+			setData(text);
+		})();
+
+		// setData("text");
+
+	});
+
+	return (
+		<div className='textContainer'>
+			<h1>
+				{data}
+			</h1>
+		</div>
+	);
+}
+
 function App() {
 
 	return (
-		<WelcomeText />
+		<>
+			<WelcomeText />
+			<hr />
+			<ApiText />
+		</>
 	)
 }
 
